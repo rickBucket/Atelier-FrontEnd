6948c5052b2e807cbf4042ebbe346d569009e962
Ricky Zhao <zrickyzhao@gmail.com>
2021-02-26 15:51:48 -0800

    file directory updates

--- testing/index.test.jsx
+++ testing/index.test.jsx
@@ -3,11 +3,11 @@ import { shallow, mount, render, configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import App from '../client/src/app.jsx';
 
+// FOR COMMON / GENERAL TESTING (SHARED COMPONENTS??)
+
 describe('<App />', () => {
   it('renders a div component', () => {
     const wrapper = shallow(<App />);
     expect(wrapper.find('#data')).not.toBeUndefined();
   });
 });
-
-

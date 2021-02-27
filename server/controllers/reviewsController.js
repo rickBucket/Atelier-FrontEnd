315b74bf61525c9460e40d3f8b991cff9e5a1997
Patrick McConnell <pmcconnell13@gmail.com>
2021-02-26 22:33:47 -0800

    Worked on setting up GET requests for routers. They are throwing errors even though they are set up nearly identical to the product request. Need to revisit tomorrow with fresh eyes.

---
+++ server/controllers/reviewsController.js
@@ -0,0 +1,42 @@
+const axios = require('axios');
+const config = require('../../config.js');
+
+const getReviews = (req, res) => (
+  axios.get('https://app-hrsei-api.herokuapp.com/api/fec2/hr-sfo/reviews', {
+    headers: {
+      'User-Agent': 'request',
+      Authorization: config.API_KEY,
+    },
+  })
+    .then((results) => {
+      console.log(results.data);
+      res.status(200).send(results.data);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(404);
+    })
+);
+
+const getReviewsMeta = (req, res) => (
+  axios.get('https://app-hrsei-api.herokuapp.com/api/fec2/hr-sfo/reviews/meta', {
+    headers: {
+      'User-Agent': 'request',
+      Authorization: config.API_KEY,
+    },
+  })
+    .then((results) => {
+      console.log(results.data);
+      res.status(200).send(results.data);
+    })
+    .catch((err) => {
+      console.log('REQUEEEEST', req);
+      console.log(err);
+      res.status(404);
+    })
+);
+
+module.exports = {
+  getReviews: getReviews,
+  getReviewsMeta: getReviewsMeta
+};

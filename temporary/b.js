8deeb9b274e3cb6a3089b5234eb7b510705685f6
Ricky Zhao <zrickyzhao@gmail.com>
2021-02-26 17:04:19 -0800

    git workflow testing files

---
+++ temporary/b.js
@@ -0,0 +1,40 @@
+/**
+ * Implement these functions following the node style callback pattern
+ */
+
+var fs = require('fs');
+var request = require('needle');
+
+// This function should retrieve the first line of the file at `filePath`
+var pluckFirstLineFromFile = function (filePath, callback = () => {}) {
+  // TODO
+  fs.readFile(filePath, 'UTF-8', (err, data)=> {
+    if (err) {
+      callback(err, null);
+    } else {
+      callback(null, data.split('\n').shift());
+    }
+  });
+};
+
+// This function should retrieve the status code of a GET request to `url`
+var getStatusCode = function (url, callback = () => {}) {
+  // TODO
+
+  request.get(url, (err, response, body) => {
+    if (err) {
+      callback(err);
+    } else {
+      callback(null, response.statusCode);
+    }
+  });
+
+};
+
+
+
+// Export these functions so we can test them and reuse them in later exercises
+module.exports = {
+  getStatusCode: getStatusCode,
+  pluckFirstLineFromFile: pluckFirstLineFromFile
+};

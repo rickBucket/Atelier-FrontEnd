dcdf678fe65992409e603888788c1366afe3de2b
Patrick McConnell <pmcconnell13@gmail.com>
2021-02-27 22:45:04 -0800

    Deleted consolelog in reviewList

--- client/src/components/ratingsAndReviews/reviewList/reviewList.jsx
+++ client/src/components/ratingsAndReviews/reviewList/reviewList.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import ReviewListEntry from './reviewListEntry.jsx'
 
 const reviewList = (props) => {
-console.log(props.reviewList)
   return(
     <div>
       Review List Component

8329f5a19635c9e0a55f9be3bf4de8d65c802675
Dana Walsh <dana.e.walsh@gmail.com>
2021-02-28 14:23:48 -0800

    Rendered actual product info into skeleton components based off of structured dummydata

--- client/src/components/relatedProducts/relatedProductsMainView.jsx
+++ client/src/components/relatedProducts/relatedProductsMainView.jsx
@@ -1,21 +1,30 @@
 /* eslint-disable */
 import React from 'react';
 import RelatedProductsList from './relatedProductsList/relatedProductsList.jsx';
+import dummyData from './dummyData.js';
 
 class RelatedProductsMainView extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-
+      relatedProducts: dummyData.related
     }
     // bind all functions here
   }
 
+  componentDidMount() {
+    // Axios get request for related products array
+    // Assign that array to state
+      // For each id in that array send a get request to products/produc-id/styles & products/product-id
+      // add the relevant info to the relatedProductsArray in state
+      // this array will be passed down to children components
+  }
+
   render() {
     return (
-      <div>
+      <div className="flexbox-container">
         <div> Hello from MainView! </div>
-        <RelatedProductsList />
+        <RelatedProductsList relatedProducts={this.state.relatedProducts}/>
       </div>
 
     )

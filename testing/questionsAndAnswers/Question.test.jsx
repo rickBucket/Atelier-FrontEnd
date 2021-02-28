405ae1f26bfbb6de0a6ba358f6610a2ced16a248
Zachary Medellin <ZacharyMedellin44@gmail.com>
2021-02-27 23:45:02 -0800

    fixed typo

--- testing/questionsAndAnswers/Question.test.jsx
+++ testing/questionsAndAnswers/Question.test.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { shallow, mount, render, configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import Question from '../../client/src/components/questionsAndAnswers/Question.jsx';
-import dummyData from '../../client/src/components/questionsAndAnswers/dummydata.js';
+import dummyData from '../../client/src/components/questionsAndAnswers/dummyData.js';
 
 describe('<Question />', () => {
   it('renders a div component', () => {

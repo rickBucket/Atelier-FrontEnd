f229d19c3827431ba03e1ce2a4eac8e9b1ae4b9f
Ricky Zhao <zrickyzhao@gmail.com>
2021-02-27 16:14:22 -0800

    fixed a typo

--- client/src/components/productDetail/productMainView.jsx
+++ client/src/components/productDetail/productMainView.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ProductInfo from './information/productInfo.jsx';
-import ProductDescription from './information/ProductDescription.jsx';
+import ProductDescription from './information/productDescription.jsx';
 import ProductShowcase from './gallery/productShowcase.jsx';
 import StyleSelector from './customization/styleSelector.jsx';
 import Checkout from './checkout/checkout.jsx';

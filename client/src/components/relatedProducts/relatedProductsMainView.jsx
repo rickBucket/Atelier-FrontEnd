2d1f2a971c7561a8c55521784fa9c83f37fb91ef
Dana Walsh <dana.e.walsh@gmail.com>
2021-02-28 22:10:36 -0800

    Replaced dummydata with API provided data

--- client/src/components/relatedProducts/relatedProductsMainView.jsx
+++ client/src/components/relatedProducts/relatedProductsMainView.jsx
@@ -1,6 +1,7 @@
 /* eslint-disable */
 import React from 'react';
 import styled from 'styled-components';
+import axios from 'axios';
 import RelatedProductsList from './relatedProductsList/relatedProductsList.jsx';
 import dummyData from './dummyData.js';
 
@@ -13,18 +14,25 @@ class RelatedProductsMainView extends React.Component {
     // bind all functions here
   }
 
-  // componentDidMount() {
-  //   // Axios get request for related products array
-  //   // Assign that array to state
-  //     // For each id in that array send a get request to products/produc-id/styles & products/product-id
-  //     // add the relevant info to the relatedProductsArray in state
-  //     // this array will be passed down to children components
-  // }
+  componentDidMount() {
+    // Axios get request for related products array
+    // Assign that array to state
+      // For each id in that array send a get request to products/produc-id/styles & products/product-id
+      // add the relevant info to the relatedProductsArray in state
+      // this array will be passed down to children components
+    axios.get(`/products/?product_id=${this.props.productID}&flag=related`)
+      .then(({data})=> {
+        console.log(data);
+        this.setState({
+          relatedProducts: data
+        })
+      })
+  }
 
   render() {
     return (
       <div >
-        <RelatedProductsList relatedProducts={this.state.relatedProducts}/>
+        <RelatedProductsList productID={this.props.productID} relatedProducts={this.state.relatedProducts}/>
       </div>
 
     )

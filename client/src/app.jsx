3d6954c73472b76e113e7c842e5ce6ec8f8df33c
Ricky Zhao <zrickyzhao@gmail.com>
2021-03-01 23:04:28 -0800

    updated styling with hovering gallery and also implemented a next product button for rerendering page with another product

--- client/src/app.jsx
+++ client/src/app.jsx
@@ -13,24 +13,31 @@ class App extends React.Component {
     this.state = {
       widget_id: '0', // 1 = product detail, 2 = related product, 3 = q&a, 4 = reviews
       productID: '',
-      loadedID: 0
+      loadedID: 0,
+      productIndex: 0
     };
     this.widgetSelect = this.widgetSelect.bind(this);
+    this.nextProduct = this.nextProduct.bind(this);
+    this.fetchProductID = this.fetchProductID.bind(this);
   }
 
 // adding component did mount to choose productID
   componentDidMount() {
-    axios.get('/products/?count=3')
+    this.fetchProductID();
+  }
+
+  fetchProductID() {
+    axios.get('/products/?count=20')
       .then(({data})=> {
         this.setState({
-          productID: data[1].id,
+          productID: data[this.state.productIndex].id,
           loadedID: 1
         });
       })
       .catch((error)=> {
         console.log('Error setting productID in App', error)
       });
-  };
+  }
 
   widgetSelect(e) {
     e.preventDefault();
@@ -39,10 +46,20 @@ class App extends React.Component {
     });
   }
 
+  nextProduct(e) {
+    e.preventDefault();
+    this.setState({
+      productIndex: (this.state.productIndex + 1)%20,
+      loadedID: 0
+    });
+    this.fetchProductID();
+  }
+
   render() {
     return (
       <div>
         <button type="submit" id="clear" value="0" onClick={this.widgetSelect}>CLEAR</button>
+        <button type="submit" id="clear" value="0" onClick={this.nextProduct}>Next Product</button>
         {
           this.state.loadedID === 1 &&
           <div>
@@ -52,19 +69,19 @@ class App extends React.Component {
             <button type="submit" value="4" onClick={this.widgetSelect}>Ratings and Reviews</button>
         </div>
         } {
-          this.state.widget_id === '1' &&
+          this.state.loadedID === 1 && this.state.widget_id === '1' &&
           <div className="productDetail">
             <ProductMainView productID={this.state.productID}/>
           </div>
         } {
-          this.state.widget_id === "2" &&
+          this.state.loadedID === 1 && this.state.widget_id === "2" &&
           // <div>RENDER RELATED PRODUCTS HERE</div>
           <RelatedProductsMainView productID={this.state.productID}/>
         } {
-          this.state.widget_id === "3" &&
+          this.state.loadedID === 1 && this.state.widget_id === "3" &&
           <QuestionMaster productID={this.state.productID}/>
         } {
-          this.state.widget_id === "4" &&
+          this.state.loadedID === 1 && this.state.widget_id === "4" &&
           <div>
             <RatingsApp productID={this.state.productID}/>
           </div>

6a3cf7a0996f09fbbb4959392d7b1e895a950020
Dana Walsh <dana.e.walsh@gmail.com>
2021-03-01 21:51:41 -0800

    Implemented beginnings of Comparison Modal

---
+++ testing/relatedProducts/relatedProductsList/comparisonModal.test.js
@@ -0,0 +1,14 @@
+/* eslint-disable */
+
+import React from 'react';
+import { shallow, mount, render, configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import ComparisonModal from '../../../client/src/components/relatedProducts/relatedProductsList/comparisonModal.jsx';
+import dummyData from '../../../client/src/components/relatedProducts/dummyData.js';
+
+describe('<ComparisonModal />', () => {
+  it('renders a div component', () => {
+    const wrapper = shallow(<ComparisonModal />);
+    expect(wrapper.find('#data')).not.toBeUndefined();
+  });
+});

700a2419eb1c3cfc440cb1f6d056fc99689d2072
Ricky Zhao <zrickyzhao@gmail.com>
2021-03-02 13:46:37 -0800

    implemented style selection

--- client/src/components/productDetail/gallery/primaryImageView.jsx
+++ client/src/components/productDetail/gallery/primaryImageView.jsx
@@ -31,7 +31,7 @@ class PrimaryImageView extends React.Component {
 
   render() {
     return (
-      <Div onClick={this.props.clicker}>
+      <Div>
         <Img src={this.props.photo} a=''></Img>
       </Div>
     );

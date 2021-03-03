56c57b446759ed08e4f75e7cca4e7c4a4bb39b65
Ricky Zhao <zrickyzhao@gmail.com>
2021-03-02 22:14:39 -0800

    actually fixed tests

--- testing/productDetail/checkout/checkout.test.jsx
+++ testing/productDetail/checkout/checkout.test.jsx
@@ -6,7 +6,9 @@ import Checkout from '../../../client/src/components/productDetail/checkout/chec
 
 describe('<Checkout />', () => {
   it('renders a div component', () => {
-    const wrapper = shallow(<Checkout />);
+    const wrapper = shallow(<Checkout styles={[{style_id: 'a', name: 'b', original_price: 'c', sale_price: 'd', 'default?': 'e', photos: [{thumbnail_url: 'f', url: 'g'}], skus: 'h'}]}
+    selectedStyle={{style_id: 'a', name: 'b', original_price: 'c', sale_price: 'd', 'default?': 'e', photos: [{thumbnail_url: 'f', url: 'g'}], skus: 'h'}}
+    />);
     expect(wrapper.find('#data')).not.toBeUndefined();
   });
 });

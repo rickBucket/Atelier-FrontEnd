f00bc69e445336be3b8968e17e05df9bbb73ee33
Patrick McConnell <pmcconnell13@gmail.com>
2021-03-03 20:54:51 -0800

    Fixed test

--- testing/ratingsAndReviews/sortOptionsTests/sortOptions.test.jsx
+++ testing/ratingsAndReviews/sortOptionsTests/sortOptions.test.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { shallow, mount, render, configure } from 'enzyme';
 import SortOptions from '../../../client/src/components/ratingsAndReviews/sortOptions/sortOptions.jsx';
+import metaDummy from '../../../client/src/components/ratingsAndReviews/metaDummy.jsx'
 
 describe('<SortOptions />', () => {
   it('should render a div', () => {
-    const wrapper = shallow(<SortOptions />);
+    const wrapper = shallow(<SortOptions metaData={metaDummy}/>);
     expect(wrapper.find('#data')).not.toBeUndefined();
   });
 });

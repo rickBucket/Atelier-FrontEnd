b7ff6327daf48e0706433cbaa8ee9218c68b60f0
Patrick McConnell <pmcconnell13@gmail.com>
2021-03-07 17:47:42 -0800

    Random ESlint

--- client/src/components/ratingsAndReviews/sortOptions/sortOptions.jsx
+++ client/src/components/ratingsAndReviews/sortOptions/sortOptions.jsx
@@ -4,12 +4,12 @@ const optionsBar = {
   width: '25%',
   border: 'none',
   textAlign: 'center',
-  cursor: 'pointer'
-}
+  cursor: 'pointer',
+};
 
 class SortOptions extends React.Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.totalReviews = this.totalReviews.bind(this);
   }
 
@@ -21,8 +21,9 @@ class SortOptions extends React.Component {
   render() {
     return(
       <div style={{
-        fontWeight: 'bold'
-      }}>{`${this.totalReviews(this.props.metaData.recommended)} reviews, sorted by most`}
+        fontWeight: 'bold',
+      }}>
+        {`${this.totalReviews(this.props.metaData.recommended)} reviews, sorted by most`}
         <select style={optionsBar}>
           <option value="0">Relevant</option>
           <option value="1">Helpful</option>

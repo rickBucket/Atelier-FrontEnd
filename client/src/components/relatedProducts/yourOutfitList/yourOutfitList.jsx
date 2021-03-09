04ae87dd7b1511f1989f14ec0eb883f220bdb1ff
Dana Walsh <dana.e.walsh@gmail.com>
2021-03-09 11:51:53 -0800

    Implemented scrolling in yourOutfit only when there are enough cards to overflow

--- client/src/components/relatedProducts/yourOutfitList/yourOutfitList.jsx
+++ client/src/components/relatedProducts/yourOutfitList/yourOutfitList.jsx
@@ -14,10 +14,16 @@ class YourOutfitList extends React.Component {
       parentProductStyles: '',
       parentProductInfo: '',
       outfits: [],
-      outfitsLoaded: false
+      outfitsLoaded: false,
+      imagesToTheRight: false,
+      imagesToTheLeft: false,
+      cardOverflow: false
     }
     this.addOutfit = this.addOutfit.bind(this);
     this.deleteOutfit = this.deleteOutfit.bind(this);
+    this.scrollLeft = this.scrollLeft.bind(this);
+    this.scrollRight = this.scrollRight.bind(this);
+    this.isOverflowing = this.isOverflowing.bind(this);
   }
 
   componentDidMount() {
@@ -44,6 +50,8 @@ class YourOutfitList extends React.Component {
           })
         })
     }
+
+    this.isOverflowing();
   }
 
   addOutfit() {
@@ -74,6 +82,7 @@ class YourOutfitList extends React.Component {
         })
       })
     }
+    this.isOverflowing();
   }
 
   deleteOutfit(productID) {
@@ -97,30 +106,82 @@ class YourOutfitList extends React.Component {
         }
       })
     })
+    this.isOverflowing();
+  }
+
+  scrollLeft() {
+    this.setState({
+      imagesToTheRight: true
+    })
+    const carousel = document.getElementById('yourOutfit');
+    const scrollLeftMax = carousel.scrollWidth - carousel.clientWidth;
+    carousel.scrollLeft -= 316;
+    if (carousel.scrollLeft <= 316) {
+      this.setState({
+        imagesToTheLeft: false,
+      });
+    }
+  }
+
+  scrollRight() {
+    this.setState({
+      imagesToTheLeft: true
+    })
+    const carousel = document.getElementById('yourOutfit');
+    const amountLeftToScroll = carousel.scrollWidth - carousel.clientWidth;
+    carousel.scrollLeft += 316;
+    if (carousel.scrollLeft >= amountLeftToScroll - 316) {
+      this.setState({
+        imagesToTheRight: false,
+      });
+    }
+  }
+
+  isOverflowing() {
+    const carousel = document.getElementById('yourOutfit');
+    const bool = carousel.scrollWidth > carousel.clientWidth;
+    this.setState({
+      cardOverflow: bool,
+      imagesToTheRight: bool
+    })
   }
 
   render() {
     return (
-      <ListContainer>
-        <CardContainer onClick={this.addOutfit} id="addOutfit">
-          <AddOutfitContent>
-            + Add To Your Outfit
+      <>
+        {this.state.imagesToTheRight ?
+          <RightButtonWrapper>
+            <RightButton onClick={this.scrollRight}>
+              &#8250;
+      </RightButton>
+          </RightButtonWrapper> : null
+        }
+        <ListContainer id="yourOutfit">
+          <CardContainer onClick={this.addOutfit} id="addOutfit">
+            <AddOutfitContent>
+              + Add To Your Outfit
 
           </AddOutfitContent>
-          <BorderDiv></BorderDiv>
-        </CardContainer>
-        {this.state.outfitsLoaded ?
+            <BorderDiv></BorderDiv>
+          </CardContainer>
+          {this.state.outfitsLoaded ?
 
-          <>
-          {this.state.outfits.map((outfit, i)=> {
-             return <YourOutfitCard
-              outfit={outfit}
-              deleteOutfit={this.deleteOutfit}
-              key={i} />
-          })}
-        </>
-         : null}
-      </ListContainer>
+            <>
+              {this.state.outfits.map((outfit, i) => {
+                return <YourOutfitCard
+                  outfit={outfit}
+                  deleteOutfit={this.deleteOutfit}
+                  key={i} />
+              })}
+            </>
+            : null}
+        </ListContainer>
+        {this.state.imagesToTheLeft ? <LeftButtonWrapper>
+          <LeftButton onClick={this.scrollLeft}>
+            &#8249;
+        </LeftButton>
+        </LeftButtonWrapper> : null}
+      </>
     )
   }
 }
@@ -146,5 +207,59 @@ const AddOutfitContent = styled.div`
   background: rgba(255,255,255,0.1);
   &:hover {
     opacity: 0.8
+  }
+`;
+
+const LeftButtonWrapper = styled.div`
+  position: absolute;
+  left: 1%;
+  top: 0px;
+  padding-left: 60px;
+  height: 89%;
+  border: none;
+  cursor: pointer;
+  z-index: 0;
+  outline: 0;
+`;
+
+const RightButtonWrapper = styled.div`
+  position: absolute;
+  right: -1%;
+  top: 0px;
+  padding-left: 60px;
+  height: 89%;
+  border: none;
+  cursor: pointer;
+  z-index: 1;
+  outline: 0;
+`;
+
+const LeftButton = styled.button`
+  position: absolute;
+  left: 2%;
+  top: 25%;
+  background-color: white;
+  border: 1px solid black;
+  cursor: pointer;
+  z-index: 10;
+  font-size: 40px;
+  &:hover {
+    background-color: black;
+    color: white;
+  }
+`;
+
+const RightButton = styled.button`
+  position: absolute;
+  right: 2%;
+  top: 25%;
+  background-color: white;
+  border: 1px solid black;
+  cursor: pointer;
+  z-index: 10;
+  font-size: 40px;
+  &:hover {
+    background-color: black;
+    color: white;
   }
 `;

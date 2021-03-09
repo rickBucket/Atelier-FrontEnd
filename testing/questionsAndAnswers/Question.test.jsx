6bc17673045ce3cbae52ca693baa0d3358381c0f
Zachary Medellin <ZacharyMedellin44@gmail.com>
2021-03-09 15:25:20 -0800

    added one working test to each file

--- testing/questionsAndAnswers/Question.test.jsx
+++ testing/questionsAndAnswers/Question.test.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import { shallow, mount, render, configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
-import QuestionMaster from '../../client/src/components/questionsAndAnswers/QuestionMaster.jsx';
+import Question from '../../client/src/components/questionsAndAnswers/Question.jsx';
 
 describe('<QuestionMaster />', () => {
   it('renders a div component', () => {
-    const wrapper = shallow(<QuestionMaster item={{ question: [] }} />);
+    const wrapper = shallow(<Question item={{answers: [] }} />);
     expect(wrapper.find('#data')).not.toBeUndefined();
   });
 });
